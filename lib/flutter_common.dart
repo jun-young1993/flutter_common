586096f1ef0621938b49c72364070888c92b7723
juny <juny3738@gmail.com>
2025-07-23 15:17:58 +0900

    init

--- lib/flutter_common.dart
+++ lib/flutter_common.dart
@@ -5,3 +5,4 @@ export 'providers/index.dart';
 export 'repositories/index.dart';
 export 'app_navigation.dart';
 export 'common_il8n.dart';
+export 'package:easy_localization/easy_localization.dart';
--- lib/providers/index.dart
+++ lib/providers/index.dart
@@ -1,3 +1,4 @@
 export 'common_bloc_provider.dart';
 export 'common_provider.dart';
 export 'common_repository_provider.dart';
+export 'base/common_bloc.dart';
