f33eb3442192650742e2ae34c5c8f120637bc624
juny <juny3738@gmail.com>
2025-08-19 17:48:10 +0900

    v1.0.37

---
+++ lib/state/payment_schedule/payment_schedule_page_bloc.dart
@@ -0,0 +1,76 @@
+import 'package:flutter_common/models/payment_schedule/payment_schedule.dart';
+import 'package:flutter_common/repositories/payment_schedule_repository.dart';
+import 'package:flutter_common/state/payment_schedule/payment_schedule_bloc.dart';
+import 'package:flutter_common/state/payment_schedule/payment_schedule_event.dart';
+import 'package:flutter_common/state/payment_schedule/payment_schedule_state.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
+
+sealed class PaymentSchedulePageEvent {}
+
+final class FetchNextPaymentSchedule extends PaymentSchedulePageEvent {
+  final String loanId;
+  FetchNextPaymentSchedule(this.loanId);
+}
+
+final class ClearPaymentSchedule extends PaymentSchedulePageEvent {}
+
+final class ChangeOrder extends PaymentSchedulePageEvent {
+  final String order; // 'ASC' 또는 'DESC'
+  ChangeOrder(this.order);
+}
+
+class PaymentSchedulePageBloc
+    extends Bloc<PaymentSchedulePageEvent, PagingState<int, PaymentSchedule>> {
+  final PaymentScheduleRepository paymentScheduleRepository;
+  String _currentOrder = 'ASC';
+  String _currentLoanId = '';
+
+  PaymentSchedulePageBloc({required this.paymentScheduleRepository})
+      : super(PagingState()) {
+    on<ClearPaymentSchedule>((event, emit) {
+      _currentOrder = 'ASC';
+      _currentLoanId = '';
+      emit(PagingState());
+    });
+
+    on<ChangeOrder>((event, emit) async {
+      _currentOrder = event.order;
+      // 정렬 순서가 변경되면 기존 리스트를 클리어하고 다시 로드
+      emit(PagingState());
+      add(FetchNextPaymentSchedule(_currentLoanId));
+    });
+
+    on<FetchNextPaymentSchedule>((event, emit) async {
+      if (state.isLoading) return;
+
+      _currentLoanId = event.loanId;
+
+      emit(state.copyWith(isLoading: true, error: null));
+      try {
+        final newKey = (state.keys?.last ?? 0) + 1;
+        final paymentSchedules =
+            await paymentScheduleRepository.findAllByLoanId(
+          event.loanId,
+          skip: (newKey - 1) * 10,
+          take: 10,
+          order: _currentOrder,
+        );
+
+        final isLastPage = paymentSchedules.isEmpty;
+
+        emit(state.copyWith(
+          keys: [...?state.keys, newKey],
+          pages: [...?state.pages, paymentSchedules],
+          hasNextPage: !isLastPage,
+          isLoading: false,
+        ));
+      } catch (e) {
+        emit(state.copyWith(error: e, isLoading: false));
+      }
+    });
+  }
+
+  // 현재 정렬 순서 getter
+  String get currentOrder => _currentOrder;
+}
